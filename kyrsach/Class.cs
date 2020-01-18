677dac3e698c420c47138c2a02fbc76280ed46f1
sashka9poltorashka <47212608+sashka9poltorashka@users.noreply.github.com>
2020-01-18 12:09:39 +0300

    Add files via upload

---
+++ kyrsach/Class.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Enterprise
+    {
+        public string name, type_of_property;
+        public int workers, registration_day, registration_month, registration_year;
+        public float anual_salary;
+        public bool leading;
+
+        public int day_of_delivery, month_of_delivery, year_of_delivery, id_of_delivery;
+        public float volume_of_delivery, cost_price, profit;
+
+        public List<Product> products;
+
+        public Enterprise()
+        {
+            int buff;
+            //string str_buff;
+
+            Console.WriteLine("Введите название предприятия: ");
+            name = Console.ReadLine();
+
+            int caseSwitch = 0;
+            Console.WriteLine("Выберите вид собственности:");
+
+            Console.WriteLine("1. Колективная");
+            Console.WriteLine("2. Кооперативная");
+            Console.WriteLine("3. ООО");
+            Console.WriteLine("4. АО");
+            Console.WriteLine("5. Трудовая");
+            caseSwitch = int.Parse(Console.ReadLine());
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    name = "Колективное предприятие " + name;
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    name = "Кооперативное предприятие " + name;
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    name = "ООО " + name;
+                    break;
+                case 4:
+                    Console.WriteLine("Case 4");
+                    name = "AO " + name;
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5");
+                    name = "Трудовое предприятие " + name;
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            Console.WriteLine("Введите день регистрации предприятия: ");
+            registration_day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц регистрации предприятия: ");
+            registration_month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год регистрации предприятия: ");
+            registration_year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Является ли передовым(1-да,0-нет): ");
+            buff = int.Parse(Console.ReadLine());
+            if (buff == 1)
+            {
+                leading = true;
+            }
+            else
+            {
+                leading = false;
+            }
+
+            Console.WriteLine("Сколько рабочих на производстве ");
+            workers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сколько типов продукции имеется: ");
+            buff = int.Parse(Console.ReadLine());
+            products = new List<Product>(buff);
+            for (int i = 0; i < buff; i++)
+            {
+                products.Add(new Product());
+            }
+
+        }
+
+        public Enterprise(string name, int registration_day, int registration_month, int registration_year, bool leading)
+        {
+            this.name = name;
+            this.registration_day = registration_day;
+            this.registration_month = registration_month;
+            this.registration_year = registration_year;
+            this.leading = leading;
+        }
+
+
+
+        public void Enterprise_info()
+        {
+            Console.WriteLine("Информация о Компании:");
+            Console.WriteLine($"Имя:{name}");
+            Console.WriteLine($"Дата регистрации:{registration_day}.{registration_month}.{registration_year}");
+            Console.WriteLine($"Количество рабочих:{workers}");
+            Console.WriteLine($"Ежегодная выручка: {profit * 12}");
+            if (leading == true)
+            {
+                Console.WriteLine("Является передовым");
+            }
+            else
+            {
+                Console.WriteLine("Не является передовым");
+            }
+            Console.WriteLine("Основной вид продукции");
+            if (products != null)
+            {
+                foreach (Product item in products)
+                {
+                    if (item.main_product == true)
+                    {
+                        Console.WriteLine($"id товара-{item.id_of_product}");
+                        Console.WriteLine($"Имя- {item.product_name}");
+
+                        if (item.unit_of_measure == true)
+                        {
+                            Console.WriteLine($"Закупочная цена-{item.buying_price} за кг");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Закупочная цена-{item.buying_price} за м^3");
+                        }
+                        //Console.WriteLine(item.unit_of_measure);
+                    }
+                }
+            }
+
+            if (products != null)
+            {
+                foreach (Product item in products)
+                {
+
+                    Console.WriteLine("Информация о продукте:");
+                    Console.WriteLine($"id товара-{item.id_of_product}");
+                    Console.WriteLine($"Имя- {item.product_name}");
+
+                    if (item.unit_of_measure == true)
+                    {
+                        Console.WriteLine($"Закупочная цена-{item.buying_price} за кг");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Закупочная цена-{item.buying_price} зам ^ 3");
+                        
+                    }
+                    //Console.WriteLine(item.unit_of_measure);
+                }
+            }
+
+        }
+
+        public void Create_delivery()
+        {
+            //float buff;
+
+            Console.WriteLine("Форма доставки");
+
+            foreach (Product item in products)
+            {
+
+                Console.WriteLine("Информация о продукте:");
+                Console.WriteLine($"id товара-{item.id_of_product}");
+                Console.WriteLine($"Имя- {item.product_name}");
+
+
+            }
+
+            Console.WriteLine("Введите id продукта для доставки: ");
+            id_of_delivery = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите день доставки: ");
+            day_of_delivery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц доставки: ");
+            month_of_delivery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год доставки: ");
+            year_of_delivery = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите объём заказа: ");
+            volume_of_delivery = int.Parse(Console.ReadLine());
+
+            foreach (Product item in products)
+            {
+
+                if (id_of_delivery == item.id_of_product)
+                {
+
+                    profit = item.buying_price * volume_of_delivery;
+                    break;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    class Product
+    {
+        public int id_of_product;
+        public float buying_price;
+        public string product_name;
+        public bool unit_of_measure, main_product;
+
+        public Product()
+        {
+            int buff;
+            Console.WriteLine("Введите id продукта: ");
+            id_of_product = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите название продукта: ");
+            product_name = Console.ReadLine();
+            Console.WriteLine("Определите еденицу измерения продукта (1-кг,0-м^3): ");
+            // true=кг false=м^3
+            buff = int.Parse(Console.ReadLine());
+            if (buff == 1)
+            {
+                unit_of_measure = true;
+            }
+            else
+            {
+                unit_of_measure = false;
+            }
+            Console.WriteLine("Введите закупочную цену продукта: ");
+            buying_price = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Является ли главным продуктом?");
+            buff = int.Parse(Console.ReadLine());
+            if (buff == 1)
+            {
+                main_product = true;
+            }
+            else
+            {
+                main_product = false;
+            }
+
+            
+        }
+
+        public Product(int id_of_product, float buying_price, string product_name)
+        {
+            this.id_of_product = id_of_product;
+            this.buying_price = buying_price;
+            this.product_name = product_name;
+        }
+
+
+
+}
+
+    class Delivery
+    {
+        public int day_of_delivery, month_of_delivery, year_of_delivery;
+        public float volume_of_delivery, cost_price, profit;
+        //формула стоимости=(себесоимость за еденицу товара(значение задаётся при создании продукта в классе product_create)*объём поставки )
+        public void Create()
+        {
+
+            Console.WriteLine("Введите id продукта: ");
+            day_of_delivery = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите день доставки: ");
+            day_of_delivery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц доставки: ");
+            month_of_delivery = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год доставки: ");
+            year_of_delivery = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите объём заказа: ");
+            volume_of_delivery = int.Parse(Console.ReadLine());
+
+        }
+    }
+    
+}
---
+++ kyrsach/Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+
+    class izgotov
+    {
+
+        public string name, phone, place;
+
+        static void Main(string[] args)
+        {
+            DoSomething();
+            Console.WriteLine("Now u can type something");
+            Console.ReadLine();
+        }
+
+
+        public izgotov()
+        {
+            
+            //string str_buff;
+
+            Console.WriteLine("Введите название Фирмы изготовителя: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("\nВведите телефон Фирмы изготовителя: ");
+            phone = Console.ReadLine();
+
+            Console.WriteLine("\nВведите место размещения фирмы Фирмы изготовителя: ");
+            place = Console.ReadLine();
+
+        }
+
+        public izgotov(string name, string phone, string place)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.place = place;
+        }
+
+
+
+        public void izgotov_info()
+        {
+            Console.WriteLine("\nИнформация о Фирме изготовителе:\n");
+            Console.WriteLine($"\nНазвание: {name}");
+            Console.WriteLine($"\nТелефон: {phone}");
+            Console.WriteLine($"\nместо размещения фирмы Фирмы изготовителя: {place}");
+        }
+
+    }
+    class ralizator
+    {
+        public string name_reliz, phone_reliz, place_reliz;
+        public ralizator()
+        {
+            Console.WriteLine("\nВведите название Фирмы реализатора: ");
+            name_reliz = Console.ReadLine();
+
+            Console.WriteLine("\nВведите телефон Фирмы реализатора: ");
+            phone_reliz = Console.ReadLine();
+
+            Console.WriteLine("\nВведите место размещения фирмы Фирмы реализатора: ");
+            place_reliz = Console.ReadLine();
+
+
+        }
+        public ralizator(string name_reliz, string phone_reliz)
+        {
+            this.name_reliz = name_reliz;
+            this.phone_reliz = phone_reliz;
+
+  
+
+        }
+    public void ralizator_info()
+    {
+        Console.WriteLine("\nИнформация о Фирме реализаторе:");
+        Console.WriteLine($"\nНазвание: {name_reliz}");
+        Console.WriteLine($"\nТелефон: {phone_reliz}");
+        Console.WriteLine($"\nместо размещения фирмы Фирмы реализатора: {place_reliz}");
+        
+        }
+    }
+
+
+
+
+    class Sborka
+    {
+        public string  cpu, cpu_ghz;
+        int ram, hdd;
+        public int relise_day, relise_month, relise_year;
+        public Sborka()
+        {
+            int tip_cpu = 0;
+            Console.WriteLine("\nВыберите тип процессора: ");
+
+            Console.WriteLine("1. Ryzen");
+            Console.WriteLine("2. Cor i");
+            Console.WriteLine("3. Pentium");
+            Console.WriteLine("4. Celeron");
+            Console.WriteLine("5. Athlon");
+            tip_cpu = int.Parse(Console.ReadLine());
+            
+            switch (tip_cpu)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    cpu = "Ryzen ";
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    cpu = "Cor i ";
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    cpu = "Pentium ";
+                    break;
+                case 4:
+                    Console.WriteLine("Case 4");
+                    cpu = "Celeron ";
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5");
+                    cpu = "Athlon ";
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+            int ghz_cpu = 0;
+            Console.WriteLine("\nВыберите тип процессора: ");
+
+            Console.WriteLine("1. 3.0");
+            Console.WriteLine("2. 3.5");
+            Console.WriteLine("3. 4.0");
+            Console.WriteLine("4. 2.6");
+            Console.WriteLine("5. 999");
+            tip_cpu = int.Parse(Console.ReadLine());
+
+            switch (ghz_cpu)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    cpu_ghz = "3.0 ";
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    cpu_ghz = "3.5 ";
+                    break;
+                case 3:
+                    Console.WriteLine("Case 3");
+                    cpu_ghz = "4.0 ";
+                    break;
+                case 4:
+                    Console.WriteLine("Case 4");
+                    cpu_ghz = "2.6 ";
+                    break;
+                case 5:
+                    Console.WriteLine("Case 5");
+                    cpu_ghz = "999 ";
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+
+
+            Console.WriteLine("\nВведите кол-во ОЗУ(в гигабайтах): ");
+            ram = int.Parse(Console.ReadLine());
+
+
+
+            int hdd;
+            Console.WriteLine("\nВведите объем жесткого диска(в гигабайтах): ");
+            hdd = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nВведите день выпуска: ");
+            relise_day = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите месяц выпуска: ");
+            relise_month = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите год выпуска: ");
+            relise_year = int.Parse(Console.ReadLine());
+        }
+
+        public Sborka(int ram, int hdd, string cpu, int relise_day, int relise_month, int relise_year)
+        {
+            this.ram = ram;
+            this.hdd = hdd;
+            this.cpu = cpu;
+
+            this.relise_day = relise_day;
+            this.relise_month = relise_month;
+            this.relise_year = relise_year;
+        }
+            public void Sborka_info()
+            {
+                Console.WriteLine("\nИнформация о Сборке:\n");
+                Console.WriteLine($"\nНазвание процессора: {cpu}");
+                Console.WriteLine($"\nчастота процессора: {cpu_ghz}");
+                Console.WriteLine($"\nобъем оперативной памяти(в гигабайтах): {ram}");
+                Console.WriteLine($"\nобъем оперативной жесткого диска(в гигабайтах): {hdd}");
+                Console.WriteLine($"\nДата выпуска: {relise_day}");
+                Console.WriteLine($". {relise_month}");
+                Console.WriteLine($". {relise_year}");
+
+        }
+
+
+    }
+
+    class reliz
+    {
+        public int V;
+        public float price;
+        public int relise_day1, relise_month1, relise_year1;
+        public void reliz1()
+        {
+
+            Console.WriteLine("\nВведите объем партии: ");
+            V = int.Parse(Console.ReadLine());
+
+
+            price = V*5000;
+            Console.WriteLine("\nцена партии: " + price);
+
+
+                    
+            Console.WriteLine("\nВведите день реализации: ");
+            relise_day1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите месяц реализации: ");
+            relise_month1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nВведите год реализации: ");
+            relise_year1 = int.Parse(Console.ReadLine());
+
+
+        }
+        public reliz(int ram,int V, int hdd, string cpu, int relise_day1, int relise_month1, int relise_year1)
+        {
+            this.V = V;
+            this.price = hdd;
+
+
+            this.relise_day1 = relise_day1;
+            this.relise_month1 = relise_month1;
+            this.relise_year1 = relise_year1;
+        }
+
+
+        public void reliz_info()
+        {
+            Console.WriteLine("\nИнформация о реализации:");
+            Console.WriteLine($"\nцена партии: {price}");
+            Console.WriteLine($"\nобъем партии: {V}");
+
+            Console.WriteLine($"\nДата реализации: {relise_day1}");
+            Console.WriteLine($". {relise_month1}");
+            Console.WriteLine($". {relise_year1}");
+
+        }
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+       
